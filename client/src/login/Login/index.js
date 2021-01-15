8f5c728655f2b745c4eb0a54a8acab1cab0c6f70
DN-GUARD <77411907+DN-GUARD@users.noreply.github.com>
2021-01-15 13:04:10 +0200

    Update index.js
    Changed Wiki Address To Reflect New Pages

--- client/src/login/Login/index.js
+++ client/src/login/Login/index.js
@@ -52,7 +52,7 @@ class Login extends Component {
                                 <Trans
                                     components={[
                                         <a
-                                            href="https://github.com/AdguardTeam/AdGuardHome/wiki/Configuration#password-reset"
+                                            href="https://github.com/DN-GUARD/DNGuard_Master/wiki/Password-Reset"
                                             key="0"
                                             target="_blank"
                                             rel="noopener noreferrer"
